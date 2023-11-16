8ef231204f0d26d4ebd31c45a98fac26884cfeb9
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2023-11-16 20:34:48 +0530

    Add snackbar

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const divSearchResults = divSearch.querySelector('#search-results');
 
 let abortController = null;
 let searchedMeals = [];
+let favouriteMeals = [];
 
 txtSearch.onkeyup = async function () {
 
@@ -28,6 +29,8 @@ txtSearch.onkeyup = async function () {
     searchedMeals = results.meals;
     abortController = null;
 
+    searchedMeals.forEach(meal => meal.isFavourite = isFavouriteMeal(meal.idMeal));
+
     render();
 
 };
@@ -47,20 +50,45 @@ function render() {
 
 }
 
+
 function getMealLiElement(meal) {
 
     const li = document.createElement('li');
+
+    const favIcon = meal.isFavourite ? 'filled' : 'hollow';
 
     li.innerHTML = `<img src="${meal.strMealThumb}" alt="food-thumb" class="food-thumb">
                     <div class="food-details">
                         <div class="food-name">${meal.strMeal}</div>
                         <div class="food-category">${meal.strCategory} | ${meal.strArea}</div>
                     </div>
-                    <img src="assets/fav-hollow.png" alt="fav-icon" class="fav-icon">`;
+                    <img src="assets/fav-${favIcon}.png" alt="fav-icon" class="fav-icon">`;
+
+
+    const imgFav = li.querySelector('img.fav-icon');
+
+    imgFav.onclick = function () {
+        showSnackbar('Added to Favourites');
+    };
 
     return li;
 }
 
 
+function isFavouriteMeal(mealId) {
+
+    for (let meal of favouriteMeals) {
+        if (meal.id === mealId) return true;
+    }
+
+    return false;
+}
+
+function showSnackbar(message) {
+    const divSnackbar = document.getElementById("snackbar");
+    divSnackbar.className = "show";
+    divSnackbar.innerText = message;
+    setTimeout(function () { divSnackbar.className = divSnackbar.className.replace("show", ""); }, 2000);
+}
 
 render();
